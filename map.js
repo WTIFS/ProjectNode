7110162fd0ed267bf92c9b995f88dd28cebf213f
CYF <chenyuanfei@91jinrong.com>
2016-02-02 09:43:31 +0800

    延迟器测试

--- map.js
+++ map.js
@@ -2,3 +2,6 @@ var map = {abb:"asdf", 3:"oooo"};
 console.log(map['abb']);
 console.log(map[3]);
 console.log(map.abb);
+console.log(JSON.stringify({}||[]));
+console.log(JSON.stringify([]||{}));
+console.log(JSON.stringify(null||[]));
