3db0d9b05a2ee58e46bb3683ae875aa3bf08b7a5
chenyuanfei <chenyuanfei@91jinrong.com>
2016-05-04 17:22:09 +0800

    add object test

--- object.js
+++ object.js
@@ -2,20 +2,26 @@
  * Created by Yuanfei on 2016/4/6.
  */
 var obj = {
+    a: 0,
     asdf: 1,
     sdfg: 2,
     dfgh: 3
 };
 
-//Not working
+if (obj.a) console.log('a');
+//Not working. For each is not a function
 /*
 obj.forEach(function(key){
-    console.log(`key: ${key}, val: ${obj.key}`);
+    console.log(`key: ${key}, val: ${obj[key]}`);
 });*/
 
-console.log(obj.size);
-console.log(obj.length);
-console.log({}.size);
+console.log(obj.size); //undefined
+console.log(obj.length); //undefined
+console.log({}.size); //undefined
 
-console.log({}=={});
-console.log(JSON.stringify({})==JSON.stringify({}));
+console.log({}=={}); //false
+console.log(JSON.stringify({})==JSON.stringify({})); //true
+
+
+
+
