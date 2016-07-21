2abb22533a3f2a09faf8f8a0d914c2ea1bba2d20
chenyuanfei <chenyuanfei@91jinrong.com>
2016-07-21 11:30:33 +0800

    add myExpress

---
+++ myExpress/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function(req, res) {
+	res.send("<h1>hello Express!</h1>");
+})
+
+router.get('/:user', function(req, res, next) {
+	var user = req.params.user;
+  	res.render('index.html', { title: user });
+});
+
+module.exports = router;
